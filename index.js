39a613feeaa96ccedbdad417a02c0a4004e9cadc
Staceyann King <staceyannking@Staceyanns-MacBook-Air.local>
2026-06-17 16:27:49 -0400

    add comments

--- index.js
+++ index.js
@@ -1,26 +1,28 @@
 import express from "express";
 const app = express();
+//the api is in this variable please do not touch
 const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
+//This port variable holds our server please do not touch
 const Port = 3000
 
-const requestOptions = {
-  method: "GET",
-  
-};
 
-
-// const response = await (req, res);
-// console.log(await response.json());
-
-
+//this is test to see if connecting to the server
 app.get("/", (req,res) => {
   res.send("Hello");
 });
 
+//This function has the variables awaiting
+//url holds the apiUrl variable and the params 
 const getApi = async() =>{
+  // Wait for the API request to return a response
   const url = await fetch(apiUrl);
+  // Wait for the JSON response to be converted into JavaScript data
+  const params = await url.json();
+  // Display the API data
+  console.log(params)
 }
 
+getApi();
 // app.get("/new/:game_id", (req, res) => {
 //   const gameId = Number(req.params.game_id);
 

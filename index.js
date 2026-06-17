b72f49e3130ed1678e8671cf06c5b7f2ae9052d8
abushunderatree <jah@Ja.localdomain>
2026-06-17 12:34:00 -0400

    Index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send( 
+    '<h1 style="background-color: yellow;">Save the Squirrels</h1>'
+    
+    );
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});

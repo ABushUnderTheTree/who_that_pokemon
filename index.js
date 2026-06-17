5b0e60e28475967ff7a00269f97cc33d139fd825
abushunderatree <jah@Ja.localdomain>
2026-06-17 17:59:44 -0400

    Configured a random letter generator that prints to /new

--- index.js
+++ index.js
@@ -5,11 +5,29 @@ const apiUrl = "https://pokeapi.co/api/v2/pokemon/";
 //This port variable holds our server please do not touch
 const Port = 3000
 
+// Your ID generator function
+const game_id = (length) => {
+    let result = ' ';
+    const game_id_characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const game_id_charactersLength = game_id_characters.length;
+    for ( let i = 0; i < length; i++ ) {
+        result += game_id_characters.charAt(Math.floor(Math.random() * game_id_charactersLength));
+    }
+    return result;
+}
 
 //this is test to see if connecting to the server
-app.get("/", (req,res) => {
-  res.send("Hello");
+app.get("/", async (req,res) => {
+  try {
+  const response = await fetch(apiUrl);
+  const data = await response.json();
+  res.send(data);
+  
+  } catch (error) {
+    res.status(500).send({ error: 'Failed to fetch data' });
+  }
 });
+
 
 //This function has the variables awaiting
 //url holds the apiUrl variable and the params 
@@ -22,7 +40,19 @@ const getApi = async() =>{
   console.log(params)
 }
 
+// Send the string back to the browser
+app.get("/new", async (req, res) => { 
+  const newGameId = game_id(8); 
+    res.send(`Your new Game ID is: ${newGameId}`); 
+});
+
 getApi();
+
+app.listen(Port, () => {
+  console.log("Server is running on port 3000");
+});
+
+
 // app.get("/new/:game_id", (req, res) => {
 //   const gameId = Number(req.params.game_id);
 
@@ -47,6 +77,3 @@ app.get("/", (req, res) => {
 
 
 */
-app.listen(Port, () => {
-  console.log("Server is running on port 3000");
-});
